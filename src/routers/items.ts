129a014d40cfe64054e420cf71825f7906b2296d
Volodymyr Halchuk <luckyone1104@gmail.com>
2022-11-16 15:43:11 +0400

    add schema validation and improve error handling

--- src/routers/items.ts
+++ src/routers/items.ts
@@ -1,49 +1,82 @@
 import express from "express";
 import { prisma } from "../db";
+import { body, query, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        console.log(req.query);
-        const items = await prisma.todoListItem.findMany({
-            where: req.query
-        });
+router.get(
+    '/',
+    query('listId').optional().isString(),
+    async (req, res, next) => {
+        const errors = validationResult(req);
 
-        res.status(200).json(items);
-    } catch (error) {
-        res.status(500).json({ error })
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const items = await prisma.todoListItem.findMany({
+                where: req.query
+            });
+
+            res.status(200).json(items);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
-router.post('/', async (req, res) => {
-    try {
-        const item = await prisma.todoListItem.create({
-            data: req.body,
-        });
+router.post(
+    '/',
+    body(['listId', 'description']).isString().notEmpty(),
+    body('completed').optional().isString(),
+    body('id').not().exists(),
+    async (req, res, next) => {
+        const errors = validationResult(req);
 
-        res.status(201).json(item);
-    } catch (error) {
-        res.status(500).json({ error })
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const item = await prisma.todoListItem.create({
+                data: req.body,
+            });
+
+            res.status(201).json(item);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
-router.put('/:id', async (req, res) => {
-    try {
-        const item = await prisma.todoListItem.update({
-            data: req.body,
-            where: {
-                id: req.params.id
-            }
-        });
+router.put(
+    '/:id',
+    body(['completed', 'description']).optional().isString(),
+    body(['id', 'listId']).not().exists(),
+    async (req, res, next) => {
+        const errors = validationResult(req);
 
-        res.status(201).json(item);
-    } catch (error) {
-        res.status(500).json({ error })
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const item = await prisma.todoListItem.update({
+                data: req.body,
+                where: {
+                    id: req.params?.id
+                }
+            });
+
+            res.status(201).json(item);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
     try {
         const item = await prisma.todoList.delete({
             where: {
@@ -53,7 +86,7 @@ router.delete('/:id', async (req, res) => {
 
         res.status(200).json(item);
     } catch (error) {
-        res.status(500).json({ error })
+        next(error);
     }
 });
 
--- src/routers/lists.ts
+++ src/routers/lists.ts
@@ -1,59 +1,82 @@
 import express from "express";
 import { prisma } from "../db";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
         const lists = await prisma.todoList.findMany();
 
         res.status(200).json(lists);
     } catch (error) {
-        res.status(500).json({ error })
+        next(error);
     }
 });
 
-router.post('/', async (req, res) => {
-    try {
-        const list = await prisma.todoList.create({
-            data: req.body,
-            include: {
-                items: true
-            }
-        });
+router.post(
+    '/',
+    body('name').isString(),
+    body(['id', 'items']).not().exists(),
+    async (req, res, next) => {
+        const errors = validationResult(req);
 
-        res.status(201).json(list);
-    } catch (error) {
-        res.status(500).json({ error })
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const list = await prisma.todoList.create({
+                data: req.body,
+                include: {
+                    items: true
+                }
+            });
+
+            res.status(201).json(list);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
-router.put('/:id', async (req, res) => {
-    try {
-        const list = await prisma.todoList.update({
-            data: req.body,
-            where: {
-                id: req.params.id
-            }
-        });
+router.put(
+    '/:id',
+    body('name').isString(),
+    body(['id', 'items']).not().exists(),
+    async (req, res, next) => {
+        const errors = validationResult(req);
 
-        res.status(201).json(list);
-    } catch (error) {
-        res.status(500).json({ error })
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const list = await prisma.todoList.update({
+                data: req.body,
+                where: {
+                    id: req.params?.id
+                }
+            });
+
+            res.status(200).json(list);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
     try {
         const list = await prisma.todoList.delete({
             where: {
-                id: req.params.id
+                id: req.params?.id
             }
         });
 
         res.status(200).json(list);
     } catch (error) {
-        res.status(500).json({ error })
+        next(error);
     }
 });
 
