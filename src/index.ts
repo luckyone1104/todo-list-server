fba923aaea536f0b23c6e05c6187f836ae1799aa
Volodymyr Halchuk <luckyone1104@gmail.com>
2022-11-19 22:03:41 +0400

    add cors

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ const port = process.env.PORT || 3001;
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 server.use('/lists', lists);
 server.use('/items', items);
